9826d553de878c33a49335a47f5da2763a18306b
CalebKoch <git@calebkoch.com>
2019-10-18 17:10:33 -0500

    Converted indetation to tabs

--- profile.ps1
+++ profile.ps1
@@ -2,5 +2,5 @@ $ProfileDir = Split-Path $Profile
 $ProfileScriptsPath = "/profileScripts/"
 
 Get-ChildItem "$(Join-Path $ProfileDir $ProfileScriptsPath)*.ps1" -Exclude *.Tests.ps1 | 
-    Where-Object Extension -eq .ps1 |
-    ForEach-Object { . $_.FullName }
+	Where-Object Extension -eq .ps1 |
+	ForEach-Object { . $_.FullName }
