83f033d1857a0372c7a67a1c73295f6320a08b73
Caleb Koch <git@calebkoch.com>
2019-10-23 21:33:14 -0500

    added comment based help for Find-String
    Renamed to make it an approved verb as well

--- profileScripts/stringUtils.ps1
+++ profileScripts/stringUtils.ps1
@@ -1,4 +1,17 @@
-filter Where-String {
+filter Find-String {
+	<#
+	.SYNOPSIS
+	Selects strings from a collection based on the provided criteria.
+	.DESCRIPTION
+	Find-String searches through a collection for A string that matches the critera, provided via the other arguments.
+	.PARAMETER Contains
+	This parameter indicates that strings in the pipeline should only be returned if they contain the value. 
+	It defaults to null, which means do not filter based on the contains criteria.
+	.PARAMETER StartsWith
+	This parameter indicates that strings in the pipeline should only be returned if they start with the value. 
+	It defaults to null, which means do not filter based on the starts with criteria.
+
+	#>
 	param (
 		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
 		[String[]] $Value,
@@ -11,8 +24,10 @@ filter Where-String {
 	$Value |
 		Where-Object { $Contains -eq $null -or $_.Contains($Contains) } |
 		Where-Object { $StartsWith -eq $null -or $_.StartsWith($StartsWith) }
-
 }
+
+Set-Alias -Name Where-String -Value Find-String
+Set-Alias -Name ?s -Value Find-String
 
 filter ConvertFrom-Base64 {
 	param (
